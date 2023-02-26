e3b3b488bde65ee36db5850bded18b972d8dd1fb
SerheyPopov <serheypopov1994@gmail.com>
2023-02-26 18:25:39 +0200

    homework-6-js-part-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,23 +1,9 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
-
-const list = document.querySelector("#ingredients");
-ingredients.forEach(value => {
-  const items = document.createElement('li');
-  items.textContent = value;
-  items.classList = 'item';
-  list.append(items);
-});
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 
 
 
-
-//const items = document.querySelector("#ingredients");
-//const markup = ingredients.map((ingredient) => `<li li class= "item" > ${ ingredient }</li >`).join("");
-//items.innerHTML = markup;
+const items = document.querySelector("#ingredients");
+const markup = ingredients
+	.map((ingredient) => `<li li class= "item" > ${ingredient}</li >`)
+	.join("");
+items.innerHTML = markup;
